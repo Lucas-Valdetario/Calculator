6f888307821e73d05508e55dfd6af6ab066e9066
Lucas Valdetario Vieira <lucas.vieira@embrapii.org.br>
2025-04-25 18:35:52 -0300

    Updating

--- src/calculators/calculator_1.py
+++ src/calculators/calculator_1.py
@@ -1,8 +1,9 @@
 from typing import Dict
 from flask import request as FlaskRequest
+from src.errors.http_unprocessable_entity import HttpUnprocessableEntityError
 
 class Calculator1:
-    def calculate(self, request: FlaskRequest) -> Dict:
+    def calculate(self, request: FlaskRequest) -> Dict:# type: ignore
         body = request.json
         input_data = self.__validate_body(body)
         splited_number = input_data / 3
@@ -16,7 +17,7 @@ class Calculator1:
     
     def __validate_body(self, body: Dict) -> float:
         if "numbers" not in body:
-            raise Exception("body mal formatado!")
+            raise HttpUnprocessableEntityError("body mal formatado!")
         
         input_data = body["numbers"]
         return input_data
