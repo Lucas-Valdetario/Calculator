10816fba5e7e7831037fbe9af2566e4b416cd89e
Lucas Valdetario Vieira <lucas.vieira@embrapii.org.br>
2025-04-25 15:33:33 -0300

    updating

--- src/calculators/calculator_1.py
+++ src/calculators/calculator_1.py
@@ -15,10 +15,10 @@ class Calculator1:
         return response
     
     def __validate_body(self, body: Dict) -> float:
-        if "number" not in body:
+        if "numbers" not in body:
             raise Exception("body mal formatado!")
         
-        input_data = body["number"]
+        input_data = body["numbers"]
         return input_data
     
     def __first_process(self, first_number: float) -> float:
